ef468b73962628f0c221a2fa829ad3336f582c7f
Kamron <102734868+kamronghost@users.noreply.github.com>
2024-03-26 15:30:32 +0500

    Add site-header

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 
-const elDarkModeButton = document.querySelector('.dark-mode-button');
+const elDarkModeButton = document.querySelector('.site-header-theme-toggler');
 
 elDarkModeButton.addEventListener('click', function(){
   document.body.classList.add('dark-mode')
