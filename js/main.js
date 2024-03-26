c656a9cb28e24078db5e8e528b14b9c12863e2a4
Kamron <102734868+kamronghost@users.noreply.github.com>
2024-03-26 15:54:33 +0500

    Toggle dark-theme on site-header theme toggler click

--- js/main.js
+++ js/main.js
@@ -2,5 +2,5 @@
 const elDarkModeButton = document.querySelector('.site-header-theme-toggler');
 
 elDarkModeButton.addEventListener('click', function(){
-  document.body.classList.add('dark-mode')
+  document.body.classList.toggle('dark-mode')
 });
